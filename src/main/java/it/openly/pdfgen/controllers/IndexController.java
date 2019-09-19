3c29cfa662798b8df7221dcb52b7fa9fa4563a25
Filippo Possenti <filippo.possenti@gmail.com>
2019-09-19 11:17:25 +0100

    Broken down the index method in smaller ones

--- src/main/java/it/openly/pdfgen/controllers/IndexController.java
+++ src/main/java/it/openly/pdfgen/controllers/IndexController.java
@@ -1,5 +1,6 @@
 package it.openly.pdfgen.controllers;
 
+import it.openly.pdfgen.models.Footer;
 import it.openly.pdfgen.models.UrlToPdfRequest;
 import it.openly.pdfgen.services.PostProcessService;
 import it.openly.pdfgen.services.PuppeteerPrintService;
@@ -18,6 +19,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class IndexController {
@@ -30,33 +32,42 @@ public class IndexController {
 
     @PostMapping("/generate")
     public ResponseEntity<InputStreamResource> index(@RequestBody UrlToPdfRequest info) {
-        List<InputStream> pdfs = new ArrayList<>();
-        for(String url : info.getUrls()) {
-            InputStream data = puppeteerPrintService.toPdf(url);
-            pdfs.add(data);
-        }
-        InputStream data;
-        if(pdfs.size() > 1) {
-            ByteArrayOutputStream outPdf = new ByteArrayOutputStream();
-            postProcessService.mergePdfFiles(pdfs, outPdf);
-            data = new ByteArrayInputStream(outPdf.toByteArray());
-        }
-        else {
-            data = pdfs.get(0);
-        }
+        List<InputStream> pdfs = generateBasicPdfs(info.getUrls());
+        InputStream mergedPdf = mergePdfs(pdfs);
+        InputStream mergedPdfsWithFooter = appendFooterToPdf(info.getFooter(), mergedPdf);
+        String filename = buildPdfFileName(info.getUrls());
+        HttpHeaders headers = buildHttpHeaders(filename);
+        return new ResponseEntity<>(new InputStreamResource(mergedPdfsWithFooter), headers, HttpStatus.OK);
+    }
 
-        ByteArrayOutputStream outPdf = new ByteArrayOutputStream();
-        if(info.getFooter() != null) {
-            postProcessService.addFooter(data, info.getFooter(), outPdf);
-            data = new ByteArrayInputStream(outPdf.toByteArray());
+    private List<InputStream> generateBasicPdfs(@RequestBody List<String> urls) {
+        return urls.stream().map(url -> puppeteerPrintService.toPdf(url)).collect(Collectors.toList());
+    }
+
+    private InputStream mergePdfs(List<InputStream> pdfs) {
+        if(pdfs.size() <= 1) {
+            return pdfs.get(0);
         }
+        ByteArrayOutputStream outPdf = new ByteArrayOutputStream();
+        postProcessService.mergePdfFiles(pdfs, outPdf);
+        return new ByteArrayInputStream(outPdf.toByteArray());
+    }
+
+    private InputStream appendFooterToPdf(Footer footer, InputStream mergedPdf) {
+        if(footer == null) {
+            return mergedPdf;
+        }
+        ByteArrayOutputStream outPdf = new ByteArrayOutputStream();
+        postProcessService.addFooter(mergedPdf, footer, outPdf);
+        return new ByteArrayInputStream(outPdf.toByteArray());
+    }
+
+    private HttpHeaders buildHttpHeaders(String filename) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.parseMediaType("application/pdf"));
-        // Here you have to set the actual filename of your pdf
-        String filename = buildPdfFileName(info.getUrls());
         headers.setContentDispositionFormData(filename, filename);
         headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
-        return new ResponseEntity<>(new InputStreamResource(data), headers, HttpStatus.OK);
+        return headers;
     }
 
     private String buildPdfFileName(List<String> urls) {
